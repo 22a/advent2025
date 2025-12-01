601a446e8513fecb3becdcdc5b4ae1b8a9d8edc9
Peter Meehan <meehanp2@tcd.ie>
2025-12-01 19:40:37 +0000

    day01 brute force. maths? never heard of her

--- 01/index.ts
+++ 01/index.ts
@@ -1,5 +1,27 @@
 import { readInput } from '../utils/readInput.ts';
 const input = readInput(import.meta.url, process.argv.includes('--example'));
-
 const lines = input.split('\n');
-console.log(lines);
+
+let dial = 50;
+let finishedOnZero = 0;
+let touchedZero = 0;
+lines.forEach((line) => {
+  const dir = line[0] === 'R' ? 1 : -1;
+  const num = Number(line.slice(1));
+  for (let i = 0; i < num; i++) {
+    dial += dir;
+    if (dial === 100) {
+      dial = 0;
+    } else if (dial === -1) {
+      dial = 99;
+    }
+    if (dial === 0) {
+      touchedZero++;
+    }
+  }
+  if (dial === 0) {
+    finishedOnZero++;
+  }
+});
+console.log('Part 1:', finishedOnZero);
+console.log('Part 2:', touchedZero);
